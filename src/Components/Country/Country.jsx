8d8212c89a7452e7114c171ff14781324c8ff1de
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-10-23 20:11:15 +0600

    make responsive

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -5,6 +5,23 @@ export default function Country({ countryData }) {
   return (
     <div className="country">
       <div className="image-country">
+        <img src={countryData.flags.png} alt="" />
+      </div>
+      <div className="country-info">
+        <h1>{countryData.name.common}</h1>
+        <h3>{countryData.capital}</h3>
+        <h2>Continents:{countryData.continents}</h2>
+        <div className="border-country">
+          <p>{countryData.borders ? countryData.borders + " " : "not found"}</p>
+        </div>
+        <button className="visited-country-btn">Visited Country</button>
+      </div>
+    </div>
+  );
+}
+/* 
+<div  className="country">
+      <div className="image-country">
         <img src={countryData.flags.svg} alt="" />
       </div>
       <div className="country-info">
@@ -16,5 +33,4 @@ export default function Country({ countryData }) {
         </div>
       </div>
     </div>
-  );
-}
+*/
